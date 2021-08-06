153bc917cb41dc3fba9137386f628b3d68599741
sobaotto <you@example.com>
2021-08-06 10:57:30 +0900

    code review

--- src/api/src/routers/analysis.ts
+++ src/api/src/routers/analysis.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import { quickstart } from "../emotion-analysis";
+// import { quickstart } from "../emotion-analysis";
 
 const analysisRouter = express.Router();
 
@@ -8,30 +8,39 @@ const endPoint: string = "/analysis";
 analysisRouter
   .route(endPoint)
   .get((req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-
     res.json({ message: "GET method" });
   })
   .post(async (req, res) => {
-    const analyzedText = req.body.text;
+    const reqBody = JSON.parse(req.body);
+    const analyzedText = reqBody.text;
 
     console.log("1");
-
-    const sentiment = await quickstart(analyzedText);
+    // const sentiment = await quickstart(analyzedText);
     console.log("2");
 
-    // console.log(`Text: ${text}`);
+    console.log(`Text: ${analyzedText}`);
     // console.log(`Sentiment score: ${sentiment.score}`);
     // console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
 
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+    );
 
     res.json({
       message: `POST method`,
-      Text: analyzedText,
-      SentimentScore: sentiment.score,
-      SentimentMagnitude: sentiment.magnitude,
+      Text: "analyzedText",
+      SentimentScore: "sentiment.score",
+      SentimentMagnitude: "sentiment.magnitude",
     });
+
+    // res.json({
+    //   message: `POST method`,
+    //   Text: analyzedText,
+    //   SentimentScore: sentiment.score,
+    //   SentimentMagnitude: sentiment.magnitude,
+    // });
   });
 
 analysisRouter
